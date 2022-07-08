8efda4640b957918ef538478dae06527f156c4dc
Scott <sbferguson17@gmail.com>
2022-07-08 11:12:58 -0600

    initial pics rendered

---
+++ index.js
@@ -0,0 +1,23 @@
+//563492ad6f91700001000001161560f93ba9468a8b2a5cfd7aecf193
+
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       var data = JSON.parse(xhttp.responseText);
+       var photos = data.photos;
+       console.log(photos);
+       var container = document.getElementById('container');
+       container.innerHTML = '';
+       photos.forEach(function(pic) {
+            var picture = document.createElement('div');
+            picture.innerHTML = `
+                <img src="${pic.src.tiny}">
+            `;
+            container.appendChild(picture);
+       });
+       
+    }
+};
+xhttp.open("GET", "https://api.pexels.com/v1/search?query=nature", true);
+xhttp.setRequestHeader('Authorization', '563492ad6f91700001000001161560f93ba9468a8b2a5cfd7aecf193');
+xhttp.send();
