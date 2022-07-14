e30f654c814534429b3253f4eff64c5f31c9096c
Scott <sbferguson17@gmail.com>
2022-07-14 12:44:33 -0600

    added mobile menus w/ links

--- index.js
+++ index.js
@@ -104,17 +104,23 @@ filters.addEventListener('click', function() {
 const mobileMenu = document.querySelector('.mobile-menu');
 const cancelBtn = document.querySelector('.cancel');
 const nav = document.querySelector('nav');
-const logo = document.querySelector('.logo');  
+const logo = document.querySelector('.logo');
+const photoGrid = document.querySelector('.grid-container');
+const mobileMenuOptions = document.querySelector('.mobile-menu-options'); 
 mobileMenu.addEventListener('click', function() {
     mobileMenu.style.display = 'none';
     cancelBtn.style.display = 'block';
     nav.style.backgroundColor = 'black';
     logo.src = 'images/PicHunter-logo-black.png';
+    photoGrid.style.display = 'none';
+    mobileMenuOptions.style.display = 'block';
+
 })
 cancelBtn.addEventListener('click', function() {
     cancelBtn.style.display = 'none';
     mobileMenu.style.display = 'block';
     nav.style.backgroundColor = 'white';
     logo.src = 'images/PicHunter-logo.png';
-
+    photoGrid.style.display = 'block';
+    mobileMenuOptions.style.display = 'none';
 })
