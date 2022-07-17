8ac54291f4f791810ac7131cd70a20bfc10efe1a
Scott <sbferguson17@gmail.com>
2022-07-17 15:07:29 -0600

    load more on scroll implemented with limit 2loads

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //563492ad6f91700001000001161560f93ba9468a8b2a5cfd7aecf193
+let currentPage = 1;
 var searchForm = document.querySelector('#search-form');
 searchForm.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -11,7 +12,7 @@ searchForm.addEventListener('submit', function(e) {
         randomNumPhotos = randomNumPhotos.toFixed(1);
         var photoNum = document.querySelector('.random-photo-num');
         photoNum.textContent = `${randomNumPhotos}k`;
-        
+        currentPage++;
         console.log(photos);
         var grid = document.querySelector('.grid');
         
@@ -82,9 +83,97 @@ searchForm.addEventListener('submit', function(e) {
     searchName.textContent = searchValue;
     searchName.style.textTransform = "capitalize";
 
-    xhttp.open("GET", `https://api.pexels.com/v1/search?query=${searchValue}`, true);
+    xhttp.open("GET", `https://api.pexels.com/v1/search?query=${searchValue}&page=${currentPage}&per_page=30`, true);
     xhttp.setRequestHeader('Authorization', '563492ad6f91700001000001161560f93ba9468a8b2a5cfd7aecf193');
     xhttp.send();
+});
+
+
+window.addEventListener('scroll', () => {
+    const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+    } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight - 1 && currentPage < 6) {
+        console.log('loaded more');
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+            var data = JSON.parse(xhttp.responseText);
+            var photos = data.photos;
+            currentPage++;
+            console.log(photos);
+            var grid = document.querySelector('.grid');
+            
+            photos.forEach(function(pic) {
+                    var picture = document.createElement('div');
+                    picture.classList.add('grid-item');
+                    picture.innerHTML = `
+                        <a href="${pic.url}">
+                            <img src="${pic.src.large}" alt="${pic.alt}" width=400>
+                        </a>
+                        <div class="pic-hover-info">
+
+                            <div class="collect-like">
+                                <div class="pic-top-flex">
+                                    <img class="icon bookmark" 
+                                        src="images/bookmark.png"
+                                        alt="collect">
+                                    <img class="icon heart"
+                                        src="images/heart.png"
+                                        alt="like">
+                                </div>
+                            </div>
+
+                            <div class="artist-name">
+                                <div class="pic-bottom-flex">
+                                    <a href="${pic.photographer_url}">
+                                        <div class="artist-flex">
+                                            <img class="avatar" 
+                                                    src="https://images.pexels.com/lib/avatars/orange.png?w=40&h=40&fit=crop&dpr=1" 
+                                                    alt="artist avatar photo">
+                                            <h5>${pic.photographer}</h5>
+                                        </div>
+                                    </a>
+                                    <img class="icon download"
+                                        src="images/download-icon.png"
+                                        alt="download">
+                                </div>    
+                            </div>
+
+                        </div>
+                    `;
+                    picture.addEventListener('mouseover', function() {
+                            this.children[1].classList.add('active');
+                    });
+                    picture.addEventListener('mouseout', function() {
+                        this.children[1].classList.remove('active');
+                    });
+                        
+                grid.appendChild(picture);
+                    
+            });
+
+            var msnry = new Masonry( grid, {
+                itemSelector: '.grid-item',
+                fitWidth: true,
+                gutter: 25
+            });
+            imagesLoaded( grid ).on( 'progress', function() {
+                msnry.layout();
+            });
+            
+        };
+    };
+    var searchValue = document.querySelector('#search-bar').value;
+
+    xhttp.open("GET", `https://api.pexels.com/v1/search?query=${searchValue}&page=${currentPage}&per_page=15`, true);
+    xhttp.setRequestHeader('Authorization', '563492ad6f91700001000001161560f93ba9468a8b2a5cfd7aecf193');
+    xhttp.send();
+    }
+}, {
+    passive: true
 });
 
 
@@ -132,3 +221,5 @@ cancelBtn.addEventListener('click', function() {
     mobileMenuOptions.style.display = 'none';
     bell.style.filter = 'invert(0)';
 })
+
+console.log(currentPage);
