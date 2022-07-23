2576ece2b811fc1d7828ca63740c32329abdef0b
Scott <sbferguson17@gmail.com>
2022-07-23 17:52:42 -0600

    fin

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+ 
+    pexel_key : '563492ad6f91700001000001161560f93ba9468a8b2a5cfd7aecf193',
+    
+    words_key : 'cbf93f50c0mshe3f948be30aeef0p110453jsn29d15e377c7a',
+    
+    randomWord_key : "2048371ed5mshba2528ec33ba204p1b3964jsnb1ef3323c1b7" 
+    
+    
+   }
